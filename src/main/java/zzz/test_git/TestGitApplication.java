a17548ecd338be03433466116c568380460a8d6f
Your Name <you@example.com>
2019-07-09 14:22:30 +0800

    第一次提交 框架的搭建

---
+++ src/main/java/zzz/test_git/TestGitApplication.java
@@ -0,0 +1,13 @@
+package zzz.test_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitApplication.class, args);
+    }
+
+}
